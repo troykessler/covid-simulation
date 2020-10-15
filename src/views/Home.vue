f9c262c70f3510233c2916119a475331267f001e
Troy Kessler <troy_kessler@protonmail.com>
2020-10-16 00:03:35 +0200

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <canvas id="simulation-window"></canvas>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, onMounted } from "vue";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  setup() {
+    onMounted(() => {
+      const canvasBody: any = document.getElementById("simulation-window");
+      const canvas = canvasBody.getContext("2d");
+
+      let w = (canvasBody.width = window.innerWidth);
+      let h = (canvasBody.height = window.innerHeight);
+
+      let tick = 0;
+
+      const opts = {
+        backgroundColor: "#222",
+        particleColor: "#fcfcfc",
+        particleAmount: 40,
+        defaultSpeed: 1,
+        addedSpeed: 2,
+
+        defaultRadius: 2,
+        addedRadius: 2,
+
+        communicationRadius: 150,
+      };
+
+      const particles = [];
+
+      window.addEventListener("resize", () => {
+        w = canvasBody.width = window.innerWidth;
+        h = canvasBody.height = window.innerHeight;
+      });
+
+      function loop() {
+        window.requestAnimationFrame(loop);
+        tick++;
+
+        canvas.fillStyle = opts.backgroundColor;
+        canvas.fillRect(0, 0, w, h);
+      }
+
+      function setup() {
+        window.requestAnimationFrame(loop);
+      }
+
+      setup();
+    });
+
+    return {};
   },
 });
 </script>
