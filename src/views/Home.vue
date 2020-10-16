cb9f53f5ef5843fa0f3e247ab8498beaec5261de
Troy Kessler <troy_kessler@protonmail.com>
2020-10-16 11:15:52 +0200

    refactored setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,50 +5,53 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 
 export default defineComponent({
   setup() {
+    const canvas = ref<any>(null);
+
+    const width = ref<number | null>(null);
+    const height = ref<number | null>(null);
+
+    const tick = ref<number>(0);
+
+    const options = ref<any>({
+      backgroundColor: "#222",
+      particleColor: "#fcfcfc",
+      particleAmount: 40,
+      defaultSpeed: 1,
+      addedSpeed: 2,
+
+      defaultRadius: 2,
+      addedRadius: 2,
+
+      communicationRadius: 150,
+    });
+
+    const loop = () => {
+      window.requestAnimationFrame(loop);
+      tick.value++;
+
+      canvas.value.fillStyle = options.value.backgroundColor;
+      canvas.value.fillRect(0, 0, width.value, height.value);
+    };
+
+    const setup = () => {
+      window.requestAnimationFrame(loop);
+    };
+
     onMounted(() => {
       const canvasBody: any = document.getElementById("simulation-window");
-      const canvas = canvasBody.getContext("2d");
+      canvas.value = canvasBody.getContext("2d");
 
-      let w = (canvasBody.width = window.innerWidth);
-      let h = (canvasBody.height = window.innerHeight);
-
-      let tick = 0;
-
-      const opts = {
-        backgroundColor: "#222",
-        particleColor: "#fcfcfc",
-        particleAmount: 40,
-        defaultSpeed: 1,
-        addedSpeed: 2,
-
-        defaultRadius: 2,
-        addedRadius: 2,
-
-        communicationRadius: 150,
-      };
-
-      const particles = [];
+      width.value = canvasBody.width = window.innerWidth;
+      height.value = canvasBody.height = window.innerHeight;
 
       window.addEventListener("resize", () => {
-        w = canvasBody.width = window.innerWidth;
-        h = canvasBody.height = window.innerHeight;
+        width.value = canvasBody.width = window.innerWidth;
+        height.value = canvasBody.height = window.innerHeight;
       });
-
-      function loop() {
-        window.requestAnimationFrame(loop);
-        tick++;
-
-        canvas.fillStyle = opts.backgroundColor;
-        canvas.fillRect(0, 0, w, h);
-      }
-
-      function setup() {
-        window.requestAnimationFrame(loop);
-      }
 
       setup();
     });
