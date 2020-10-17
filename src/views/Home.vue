241178c914ce8dba957cd16f5947ee20d6de6ed9
Troy Kessler <troy.kessler99@gmail.com>
2020-10-17 19:43:58 +0200

    improved code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,131 +8,92 @@
 import { defineComponent, onMounted, ref } from "vue";
 import P5 from 'p5';
 
-enum STATUS {
-  S = 'S',
-  I = 'I',
-  R = 'R'
-}
+class People {
+  x: any
+  y: any
+  diameter: any
+  xSpeed: any
+  ySpeed: any
+  fill: any
+  radius: any
+  sick: any
 
-enum STATUS_COLOR {
-  S = '#256dd9',
-  I = '#c93030',
-  R = 'white'
-}
-
-interface IOptions {
-  width: number;
-  height: number;
-  amountParticles: number;
-  speed: number;
-  size: number;
-  i0: number;
-  infectionRadius: number;
-}
-
-class Particle {
-  x: number = 0;
-  y: number = 0;
-
-  speed: number = 0;
-  directions: number = 0;
-
-  d: { x: number, y: number } = { x: 0, y: 0 };
-
-  status: STATUS = STATUS.S;
-
-  constructor(status: STATUS, options: IOptions) {
-    this.status = status;
-
-    this.x = Math.random() * options.width;
-    this.y = Math.random() * options.height;
-
-    this.speed = options.speed + Math.random() * 2 * options.speed
-    this.directions = Math.floor(Math.random() * 360)
-    
-    this.d = {
-      x: Math.cos(this.directions) * this.speed,
-      y: Math.sin(this.directions) * this.speed
-    }
+  constructor() {
+    //all variables declared in here by this.
+    this.x = Math.random() * 500;
+    this.y = Math.random() * 500;
+    this.diameter = 3;
+    this.xSpeed = (Math.random() - 1);
+    this.ySpeed = (Math.random() - 1);
+    this.fill = "white";
+    this.radius = 3;
+    this.sick = false;
+  }
+  move() {
+    var speedMultiplier = 1;
+    this.x += this.xSpeed * speedMultiplier;
+    this.y += this.ySpeed * speedMultiplier;
+    //safety so can't go out
+    if (this.x >= 500) this.xSpeed = -this.xSpeed;
+    if (this.x <= 0) this.xSpeed = -this.xSpeed;
+    if (this.y >= 500) this.ySpeed = -this.ySpeed;
+    if (this.y <= 0) this.ySpeed = -this.ySpeed;
+  }
+  infected() {
+    //set if loop then make bool false for reset
+    this.fill = "red";
+    this.sick = true;
+  }
+  intersects(other: any) {
+    var d = Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2));
+    return d < this.radius;
+  }
+  cured() {
+    this.fill = "white";
+    this.sick = false;
   }
 }
 
 export default defineComponent({
   setup() {
-    const options = ref<IOptions>({
-      width: 500,
-      height: 500,
+    const sketch = (p5: any) => {
 
-      amountParticles: 200,
-      speed: 1,
-      size: 5,
+      let person: any;
+      //array
+      let people: any[] = [];
 
-      i0: 3,
-      infectionRadius: 10
-    })
+      let create: any = true;
 
-    const particles = ref<Particle[]>([])
+      let corona: any;
 
-    const updateParticle = (particle: Particle) => {
-      if (particle.x >= options.value.width || particle.x <= 0) {
-        particle.d.x *= -1;
-      }
-      if (particle.y >= options.value.height || particle.y <= 0) {
-        particle.d.y *= -1;
-      }
-      particle.x > options.value.width ? particle.x = options.value.width : particle.x;
-      particle.y > options.value.height ? particle.y = options.value.height : particle.y;
-      particle.x < 0 ? particle.x = 0 : particle.x;
-      particle.y < 0 ? particle.y = 0 : particle.y;
-
-      particle.x += particle.d.x;
-      particle.y += particle.d.y;
-    }
-
-    const calcDistance = (p1: Particle, p2: Particle) => {
-      return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2))
-    }
-
-    const calcConnections = (p1: Particle) => {
-      if (p1.status === STATUS.I) {
-        for (let i = 0; i < options.value.amountParticles; i++) {
-          if (particles.value[i].status === STATUS.S) {
-            const distance = calcDistance(p1, particles.value[i])
-    
-            if (distance && distance <= options.value.infectionRadius) {
-              particles.value[i].status = STATUS.I
+      function virus() {
+        people[0].infected();
+        for (var i = 0; i < people.length; i++) {
+          //runs through the array and does both functions for all objects
+          people[i].move();
+          p5.fill(people[i].fill);
+          p5.ellipse(people[i].x, people[i].y, people[i].diameter, people[i].diameter);
+        }
+        for (var i = 0; i < people.length; i++) {
+          for (var j = 0; j < people.length; j++) {
+            if (j != i && people[i].intersects(people[j]) && people[j].sick == true) {
+              people[i].infected();
             }
           }
         }
       }
-    }
 
-    const sketch = (p5: any) => {
       p5.setup = () => {
-        p5.createCanvas(options.value.width, options.value.height);
+        p5.createCanvas(500, 500);
 
-        for (let i = 0; i < options.value.amountParticles - options.value.i0; i++) {
-          particles.value.push(new Particle(STATUS.S, options.value))
-        }
-
-        for (let i = 0; i < options.value.i0; i++) {
-          particles.value.push(new Particle(STATUS.I, options.value))
+        for (let i = 0; i < 1000; i++) {
+          people.push(new People());
         }
       };
 
       p5.draw = () => {
         p5.background(33, 33, 33);
-
-        for (let i = 0; i < options.value.amountParticles; i++) {
-          updateParticle(particles.value[i])
-
-          p5.fill(STATUS_COLOR[particles.value[i].status])
-          p5.ellipse(particles.value[i].x, particles.value[i].y, options.value.size, options.value.size)
-        }
-
-        for (let i = 0; i < options.value.amountParticles; i++) {
-          calcConnections(particles.value[i])
-        }
+        virus();
       };
     }
 
@@ -140,9 +101,7 @@ export default defineComponent({
       new P5(sketch, 'simulation-window');
     })
 
-    return {
-      options
-    }
+    return {}
   }
 });
 </script>
