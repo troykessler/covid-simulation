28ee80bb942e76ad2410b7c863c79c6bdb9bde9f
Troy Kessler <troy.kessler99@gmail.com>
2020-10-17 14:38:26 +0200

    asdf

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,11 +15,13 @@ export default defineComponent({
     const height = ref<number | null>(null);
 
     const tick = ref<number>(0);
-    const particles = ref<any[]>([])
+    const susceptibles = ref<any[]>([])
+    const infected = ref<any[]>([])
+    const removed = ref<any[]>([])
 
     const options = ref<any>({
-      particleAmount: 100,
-      defaultSpeed: 1,
+      particleAmount: 200,
+      defaultSpeed: 0,
       addedSpeed: 2,
 
       defaultRadius: 1,
@@ -27,8 +29,45 @@ export default defineComponent({
       communicationRadius: Math.pow(20, 2),
 
       i0: 3,
-      r0: 500
+      r0: 200
     });
+
+    const update = (particle: any) => {
+      if (particle.x >= width.value! || particle.x <= 0) {
+        particle.directions.x *= -1;
+      }
+      if (particle.y >= height.value! || particle.y <= 0) {
+        particle.directions.y *= -1;
+      }
+
+      particle.x > width.value! ? particle.x = width.value! : particle.x;
+      particle.y > height.value! ? particle.y = height.value! : particle.y;
+
+      particle.x < 0 ? particle.x = 0 : particle.x;
+      particle.y < 0 ? particle.y = 0 : particle.y;
+
+      particle.x += particle.directions.x;
+      particle.y += particle.directions.y;
+    }
+
+    const draw = (particle: any) => {
+      function color() {
+        switch(particle.status) {
+          case 'S':
+            return "#0388fc";
+          case 'I':
+            return '#fc0328';
+          case 'R':
+            return '#c2c2c2';
+        }
+      }
+
+      canvas.value.beginPath();
+      canvas.value.arc(particle.x, particle.y, 2, 0, Math.PI * 2);
+      canvas.value.closePath();
+      canvas.value.fillStyle = color();
+      canvas.value.fill();
+    }
 
     const loop = () => {
       window.requestAnimationFrame(loop);
@@ -37,22 +76,40 @@ export default defineComponent({
       canvas.value.fillStyle = "#222";
       canvas.value.fillRect(0, 0, width.value, height.value);
 
-      for(let i = 0; i < particles.value.length; i++){
-        particles.value[i].update();
-        particles.value[i].draw();
+      for(let i = 0; i < susceptibles.value.length; i++){
+        update(susceptibles.value[i]);
+        draw(susceptibles.value[i]);
+      }
+      for(let i = 0; i < infected.value.length; i++){
+        update(infected.value[i])
+
+        infected.value[i].age++;
+
+        if (infected.value[i].age > options.value.r0) {
+          let particle = infected.value[i]
+          infected.value.splice(i, 1)
+          particle.status = 'R'
+          removed.value.push(particle)
+        }
+
+        draw(infected.value[i])
+      }
+      for(let i = 0; i < removed.value.length; i++){
+        update(removed.value[i]);
+        draw(removed.value[i]);
       }
 
-      for (let a = 0; a < particles.value.length; a++) {
-        drawConnections(particles.value[a], particles.value)
+      for (let a = 0; a < infected.value.length; a++) {
+        drawConnections(infected.value[a], susceptibles.value)
       }
     };
 
     const setup = () => {
       for(let i = 0; i < options.value.particleAmount - options.value.i0; i++){
-        particles.value.push(new Particle());
+        susceptibles.value.push(new Particle());
       }
       for(let i = 0; i < options.value.i0; i++){
-        particles.value.push(new Particle('I'));
+        infected.value.push(new Particle('I'));
       }
       window.requestAnimationFrame(loop);
     };
@@ -75,54 +132,6 @@ export default defineComponent({
       constructor(status: string = 'S') {
         this.status = status
       }
-
-      color() {
-        switch(this.status) {
-          case 'S':
-            return "#0388fc";
-          case 'I':
-            return '#fc0328';
-          case 'R':
-            return '#c2c2c2';
-        }
-      }
-
-      border() {
-        if (this.x >= width.value! || this.x <= 0) {
-          this.directions.x *= -1;
-        }
-        if (this.y >= height.value! || this.y <= 0) {
-          this.directions.y *= -1;
-        }
-
-        this.x > width.value! ? this.x = width.value! : this.x;
-        this.y > height.value! ? this.y = height.value! : this.y;
-
-        this.x < 0 ? this.x = 0 : this.x;
-        this.y < 0 ? this.y = 0 : this.y;
-      }
-
-      update() {
-        this.border()
-        this.x += this.directions.x;
-        this.y += this.directions.y;
-
-        if (this.status === 'I') {
-          this.age++;
-
-          if (this.age > this.recovery) {
-            this.status = 'R'
-          }
-        }
-      }
-
-      draw() {
-        canvas.value.beginPath();
-        canvas.value.arc(this.x, this.y, options.value.defaultRadius, 0, Math.PI * 2);
-        canvas.value.closePath();
-        canvas.value.fillStyle = this.color();
-        canvas.value.fill();
-      }
     }
 
     const calcDistance = (x1: number, y1: number, x2: number, y2: number) => Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2);
@@ -132,7 +141,7 @@ export default defineComponent({
         const distance = calcDistance(point1.x, point1.y, father[i].x, father[i].y);
         const opacity = 1 - distance / options.value.communicationRadius;
 
-        if (opacity > 0 && point1.status === 'I') {
+        if (opacity > 0) {
           canvas.value.lineWidth = opacity;
           canvas.value.strokeStyle = "rgba(255,255,255,0.5)";
           canvas.value.beginPath();
@@ -141,10 +150,11 @@ export default defineComponent({
           canvas.value.closePath();
           canvas.value.stroke();
 
-          if (father[i].status === 'S') {
-            if (Math.random() < 0.01) {
-              father[i].status = 'I'
-            }
+          if (Math.random() < 0.01) {
+            let particle =  father[i]
+            susceptibles.value.splice(i, 1)
+            particle.status = 'I'
+            infected.value.push(particle)
           }
         }
       }
