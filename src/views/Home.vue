c6ce9d35db26f2759f9337b3f25fdc1c12427ac9
Troy Kessler <troy.kessler99@gmail.com>
2020-10-18 18:54:01 +0200

    added chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    <div id="title">S = {{ susceptibles }}, I = {{ infected }}, R = {{ removed }}</div>
     <div id="simulation-window"></div>
+    <div>
+      <div id="chart" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
 import P5 from 'p5';
+import ApexCharts from 'apexcharts'
 
 enum STATUS {
   S = 'S',
@@ -17,7 +22,7 @@ enum STATUS {
 enum STATUS_COLOR {
   S = '#256dd9',
   I = '#c93030',
-  R = 'white'
+  R = '#ffffff'
 }
 
 interface IOptions {
@@ -29,6 +34,7 @@ interface IOptions {
   i0: number;
   infectionRadius: number;
   infectionRate: number;
+  recovery: number;
 }
 
 class Particle {
@@ -42,12 +48,12 @@ class Particle {
   status: STATUS = STATUS.S;
   duration: number = 0;
 
-  constructor(status: STATUS, width: number, height: number, speed: number) {
+  constructor(status: STATUS, options: IOptions) {
     this.status = status;
 
-    this.x = Math.random() * width;
-    this.y = Math.random() * height;
-    this.speed = speed + Math.random() * 2 * speed
+    this.x = Math.random() * options.width;
+    this.y = Math.random() * options.height;
+    this.speed = options.speed + Math.random() * 2 * options.speed
     this.directions = Math.floor(Math.random() * 360)
     
     this.d = {
@@ -72,10 +78,6 @@ class Particle {
 
     if (this.status === STATUS.I) {
       this.duration++;
-
-      if (this.duration > 500) {
-        this.status = STATUS.R;
-      }
     }
   }
 
@@ -92,10 +94,74 @@ export default defineComponent({
       amountParticles: 1000,
       speed: 0.5,
       size: 4,
-      i0: 3,
+      i0: 2,
       infectionRadius: 5,
-      infectionRate: 0.015
+      infectionRate: 0.02,
+      recovery: 400
     })
+
+    const chart = ref<any>(null)
+    const counter = ref<number>(0)
+
+    const susceptibles = ref<number>(options.value.amountParticles - options.value.i0)
+    const infected = ref<number>(options.value.i0)
+    const removed = ref<number>(0)
+
+    const chartOptions = ref<any>({
+      chart: {
+        height: 250,
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight',
+        width: 2
+      },
+      colors: ['#256dd9', '#c93030', '#ffffff'],
+      series: [
+        {
+          name: 'Susceptibles',
+          data: []
+        },
+        {
+          name: 'Infected',
+          data: []
+        },
+        {
+          name: 'Removed',
+          data: []
+        }
+      ]
+    })
+
+    const chartSeries: any = [
+      {
+        name: 'Susceptibles',
+        data: [susceptibles.value]
+      },
+      {
+        name: 'Infected',
+        data: [infected.value]
+      },
+      {
+        name: 'Removed',
+        data: [removed.value]
+      }
+    ]
+    
+    const updateChart = () => {
+      chartSeries[0].data = [...chartSeries[0].data, susceptibles.value]
+      chartSeries[1].data = [...chartSeries[1].data, infected.value]
+      chartSeries[2].data = [...chartSeries[2].data, removed.value]
+
+      chart.value.updateSeries(chartSeries)
+      counter.value++;
+    }
 
     const sketch = (p5: any) => {
       let particles: Particle[] = [];
@@ -106,6 +172,13 @@ export default defineComponent({
 
         for (let i = 0; i < particles.length; i++) {
           particles[i].move(options.value.width, options.value.height);
+
+          if (particles[i].status === STATUS.I && particles[i].duration > options.value.recovery) {
+            particles[i].status = STATUS.R;
+            infected.value--;
+            removed.value++;
+          }
+
           p5.fill(STATUS_COLOR[particles[i].status]);
           p5.ellipse(particles[i].x, particles[i].y, options.value.size, options.value.size);
         }
@@ -119,6 +192,8 @@ export default defineComponent({
                 if (particleI.intersects(particleJ, radius)) {
                   if (Math.random() < infectionRate) {
                     particleJ.status = STATUS.I;
+                    susceptibles.value--;
+                    infected.value++;
                   }
                 }
               }
@@ -131,34 +206,55 @@ export default defineComponent({
         p5.createCanvas(500, 500);
 
         for (let i = 0; i < options.value.amountParticles - options.value.i0; i++) {
-          particles.push(new Particle(STATUS.S, options.value.width, options.value.height, options.value.speed));
+          particles.push(new Particle(STATUS.S, options.value));
         }
 
         for (let i = 0; i < options.value.i0; i++) {
-          particles.push(new Particle(STATUS.I, options.value.width, options.value.height, options.value.speed));
+          particles.push(new Particle(STATUS.I, options.value));
         }
       };
 
       p5.draw = () => {
         p5.background(33, 33, 33);
         virus();
+
+        if (counter.value % 40 === 0) {
+          updateChart();
+        }
+
+        counter.value++;
       };
     }
 
     onMounted(() => {
       new P5(sketch, 'simulation-window');
+
+      chart.value = new ApexCharts(document.getElementById('chart'), chartOptions.value);
+      chart.value.render()
     })
 
-    return {}
+    return {
+      susceptibles,
+      infected,
+      removed
+    }
   }
 });
 </script>
 
 <style>
+#title {
+  margin: 50px auto 10px auto;
+  color: white;
+}
 #simulation-window {
   width: 500px !important;
   height: 500px !important;
   border: 2px solid white;
-  margin: 100px auto;
+  margin: 0 auto;
+}
+#chart {
+  width: 500px;
+  margin: 20px auto;
 }
 </style>
